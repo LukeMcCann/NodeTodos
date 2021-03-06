ccd7fc20c70d72473f53e80c97824a98524a8aa3
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-06 16:00:11 +0000

    Separate routes with middleware routing

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 'use strict';
 
 const app = require('express')();
-
 require('dotenv').config();
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
+const routes = require('./routes');
 
-app.get('/', (req, res) => {
-    res.render('list', {foo: 'FOO'});
-});
+app.use('/', routes);
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
